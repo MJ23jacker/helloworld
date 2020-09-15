069723701839893049dfde8913e33306069a6150
Administrator <Administrator@XJ32RJQQESJD1QX>
2020-09-15 15:16:28 +0800

    更改helloworld

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,6 +3,10 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		String str="kobe";
+		String str1="michal";
+		System.out.println("str==str1"+str==str1);
+		System.out.println(str.equals(str1));
 		System.out.println("helloworld");
 	}
 }
