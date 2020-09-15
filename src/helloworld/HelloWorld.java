affb634627cfa15acad288f77164a347c1be1024
HelloWorld <1050107814@qq.com>
2020-09-15 16:43:55 +0800

    测试idea提交更改代码

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -5,8 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		String str="kobe";
 		String str1="michal";
-		System.out.println("str==str1"+str==str1);
-		System.out.println("1111"+str.equals(str1));
+		System.out.println("str==str1"+str.equals(str1));
 		System.out.println("helloworld");
 	}
 }
